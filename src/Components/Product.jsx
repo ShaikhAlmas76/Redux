d1e1f9da062dd1590ad652d217419c72d00d8a68
ShaikhAlmas76 <shaikhalmas1484@gmail.com>
2023-11-29 17:52:41 +0530

    redux-project

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -5,7 +5,7 @@ import { useDispatch } from "react-redux";
 const Product = (props) => {
   const dispatch = useDispatch();
   return (
-    <div className="card" style={{ width: "18rem;", margin: "10px" }}>
+    <div className="card" style={{ width: "18rem;", margin: "25px" }}>
       <img className="card-img-top" src={props.image} alt={props.productName} />
       <div className="card-body">
         <h5 class="card-title">{props.productName}</h5>
